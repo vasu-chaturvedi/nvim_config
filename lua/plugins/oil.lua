4cbbb6c8fce46a049ed49cf69ca7e064c3e6c0c4
vasu-chaturvedi <109227883+vasu-chaturvedi@users.noreply.github.com>
2025-12-25 22:23:06 +0530

    Update oil.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,34 +1,42 @@
+-- lua/plugins/oil.lua
 return {
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("oil").setup({
-                columns = {
-                    "icon",
-                    "permissions",
-                    "size",
-                    { "mtime", format = "%d-%m-%Y %H:%M" },
-                },
-                keymaps = {
-                    ["<C-h>"] = false,
-                    ["<C-l>"] = false,
-
-                    ["<C-k>"] = false,
-                    ["<C-j>"] = false,
-                    ["<M-h>"] = "actions.select_split",
-                },
-                view_options = {
-                    show_hidden = true,
-                },
-            })
-
-            -- Open parent directory in current window
-
-            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
-            -- Open parent directory in floating window
-            vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+  {
+    "stevearc/oil.nvim",
+    event = "VeryLazy",                 -- keep it lazy
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- If any CLI arg is a directory, load Oil and open it there
+          for _, arg in ipairs(vim.fn.argv()) do
+            if vim.fn.isdirectory(arg) == 1 then
+              local ok, lazy = pcall(require, "lazy")
+              if ok and lazy and lazy.load then
+                lazy.load({ plugins = { "oil.nvim" } })
+              end
+              require("oil").open(arg)
+              return
+            end
+          end
         end,
+      })
+    end,
+    opts = {
+      default_file_explorer = true,
+      columns = { "icon" },
+      keymaps = {
+        ["<C-h>"] = false, ["<C-j>"] = false, ["<C-k>"] = false, ["<C-l>"] = false,
+        ["<M-h>"] = "actions.select_split",
+      },
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name) return name == ".git" end,
+      },
     },
+    keys = {
+      { "-", "<CMD>Oil<CR>", desc = "Oil: open parent directory" },
+      { "<space>-", function() require("oil").toggle_float() end, desc = "Oil: toggle float" },
+    },
+  },
 }
+
